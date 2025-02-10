b5deaad2bf2b43908fff2422d088bc9bd79cc890
Vitalii <co4ckowoi.v@gmail.com>
2025-02-10 16:28:09 +0200

    added cron

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { Telegraf } from "telegraf";
 import express from "express";
-import cron from "node-cron";
+import { CronJob } from "cron";
 import dotenv from "dotenv";
 
 import { messages } from "./messages.js";
@@ -21,7 +21,6 @@ let currentStopIndex = 0;
 
 bot.start((ctx) => ctx.reply('👋 Привет! Меня зовут Бабаченок, я узнал что ты из семьи Бабакевечей и тут же постарался тебя найти чтоб стать твои другом. В основном я сплю как и все мы бабаки, но с этого дня я специально буду просыпаться каждое утро чтоб твое утро было как можно теплее и радостнее! 🌞\n ✨ Если хочешь дружить со мной просто напиши "Давай дружить"\n С уважением, твой друг Бабаченок! 🐿'));
 
-
 app.post(`/webhook/${process.env.BOT_TOKEN}`, (req, res) => {
     bot.handleUpdate(req.body);
     res.sendStatus(200);
@@ -35,34 +34,42 @@ bot.on("text", (ctx) => {
 
         if (task) task.stop();
 
-        task = cron.schedule("10 14 * * *", () => {
-            const currentMessage = messages[currentMessageIndex];
-            const image = images[Math.floor(Math.random() * images.length)];
+        task = new CronJob(
+            "40 16 * * *",
+            function () {
+                const currentMessage = messages[currentMessageIndex];
+                const image = images[Math.floor(Math.random() * images.length)];
 
-            bot.telegram.sendPhoto(ctx.chat.id, image).then(() => {
-                bot.telegram.sendMessage(ctx.chat.id, currentMessage);
-            });
+                bot.telegram.sendPhoto(ctx.chat.id, image).then(() => {
+                    bot.telegram.sendMessage(ctx.chat.id, currentMessage);
+                });
 
-            currentMessageIndex = (currentMessageIndex + 1) % messages.length;
-        });
-
-        task.start();
+                currentMessageIndex = (currentMessageIndex + 1) % messages.length;
+            },
+            null,
+            true,
+            "Europe/Kiev"
+        );
 
     } else if (message === "я соскучился") {
         ctx.reply("🐿 Ура! Ты вернулся! Я так соскучился по тебе! Теперь я снова готов радовать тебя, поднимать настроение и быть рядом! Давай начнем новый день с улыбкой! 🌞");
 
         if (task) task.stop();
 
-        task = cron.schedule("10 14 * * *", () => {
-            const currentMessage = messages[Math.floor(Math.random() * messages.length)];
-            const image = images[Math.floor(Math.random() * images.length)];
+        task = new CronJob(
+            "40 16 * * *",
+            function () {
+                const currentMessage = messages[Math.floor(Math.random() * messages.length)];
+                const image = images[Math.floor(Math.random() * images.length)];
 
-            bot.telegram.sendPhoto(ctx.chat.id, image).then(() => {
-                bot.telegram.sendMessage(ctx.chat.id, currentMessage);
-            });
-        });
-
-        task.start();
+                bot.telegram.sendPhoto(ctx.chat.id, image).then(() => {
+                    bot.telegram.sendMessage(ctx.chat.id, currentMessage);
+                });
+            },
+            null,
+            true,
+            "Europe/Kiev"
+        );
 
     } else if (message === "стоп") {
         if (task) {
@@ -81,7 +88,6 @@ bot.on("text", (ctx) => {
         bot.telegram.sendMessage(ctx.chat.id, randomExcuse);
     }
 });
-
 
 const WEBHOOK_URL = `${process.env.SERVER_URL}/webhook/${process.env.BOT_TOKEN}`;
 bot.telegram.setWebhook(WEBHOOK_URL).then(() => {
