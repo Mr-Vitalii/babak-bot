67131420fc2d88cb643f1d432203e51768b20615
Vitalii <co4ckowoi.v@gmail.com>
2025-02-12 19:34:42 +0200

    test12

--- index.js
+++ index.js
@@ -19,7 +19,33 @@ let task = null;
 let currentMessageIndex = 0;
 let currentStopIndex = 0;
 
-bot.start((ctx) => ctx.reply('👋 Привет! Меня зовут Бабаченок, я узнал что ты из семьи Бабакевечей и тут же постарался тебя найти чтоб стать твои другом. В основном я сплю как и все мы бабаки или занимаюсь своими мелкими бабачьими делишками, но с этого дня я специально буду просыпаться каждое утро чтоб твое утро было как можно теплее и радостнее! 🌞\n ✨ Если хочешь дружить со мной просто напиши "Давай дружить"\n С уважением, твой друг Бабаченок! 🐿'));
+function startCronJob(chatId) {
+    if (task) {
+        task.stop();
+        task = null;
+    }
+
+    task = new CronJob(
+        "0 9 * * *",
+        function () {
+            const currentMessage = messages[currentMessageIndex];
+            const image = images[Math.floor(Math.random() * images.length)];
+
+            bot.telegram.sendPhoto(chatId, image).then(() => {
+                bot.telegram.sendMessage(chatId, currentMessage);
+            });
+
+            currentMessageIndex = (currentMessageIndex + 1) % messages.length;
+        },
+        null,
+        true,
+        "America/Anchorage"
+    );
+}
+
+bot.start((ctx) => {
+    ctx.reply("👋 Привет! Меня зовут Бабаченок, я узнал что ты из семьи Бабакевечей и тут же постарался тебя найти чтоб стать твои другом. В основном я сплю как и все мы бабаки, но с этого дня я специально буду просыпаться каждое утро чтоб твое утро было как можно теплее и радостнее! 🌞\n ✨ Если хочешь дружить со мной просто напиши 'Давай дружить'\n С уважением, твой друг Бабаченок! 🐿");
+});
 
 app.post(`/webhook/${process.env.BOT_TOKEN}`, (req, res) => {
     bot.handleUpdate(req.body);
@@ -28,56 +54,25 @@ app.post(`/webhook/${process.env.BOT_TOKEN}`, (req, res) => {
 
 bot.on("text", (ctx) => {
     const message = ctx.message.text.toLowerCase();
+    const chatId = ctx.chat.id;
 
     if (message === "давай дружить") {
         ctx.reply("🐿 Ура! Мы с тобой друзья! Я так рад, что ты хочешь дружить! Я обещаю делать твои утренние часы ярче и веселее. Каждое утро буду просыпаться только для того, чтобы послать тебе немного позитива! 🌞 Так что готовься, я всегда рядом, готов поделиться добрыми словами и радостью! ✨");
 
-        if (task) task.stop();
-
-        task = new CronJob(
-            "59 13 * * *",
-            function () {
-                const currentMessage = messages[currentMessageIndex];
-                const image = images[Math.floor(Math.random() * images.length)];
-
-                bot.telegram.sendPhoto(ctx.chat.id, image).then(() => {
-                    bot.telegram.sendMessage(ctx.chat.id, currentMessage);
-                });
-
-                currentMessageIndex = (currentMessageIndex + 1) % messages.length;
-            },
-            null,
-            true,
-            "Europe/Kiev"
-        );
+        startCronJob(chatId);
 
     } else if (message === "я соскучился") {
         ctx.reply("🐿 Ура! Ты вернулся! Я так соскучился по тебе! Теперь я снова готов радовать тебя, поднимать настроение и быть рядом! Давай начнем новый день с улыбкой! 🌞");
 
-        if (task) task.stop();
+        startCronJob(chatId);
 
-        task = new CronJob(
-            "59 13 * * *",
-            function () {
-                const currentMessage = messages[Math.floor(Math.random() * messages.length)];
-                const image = images[Math.floor(Math.random() * images.length)];
-
-                bot.telegram.sendPhoto(ctx.chat.id, image).then(() => {
-                    bot.telegram.sendMessage(ctx.chat.id, currentMessage);
-                });
-            },
-            null,
-            true,
-            "Europe/Kiev"
-        );
-
-    } else if (message === "не хочу с тобой дружить") {
+    } else if (message === "стоп") {
         if (task) {
             task.stop();
             task = null;
 
             const currentStopMessage = stopMsgs[currentStopIndex];
-            bot.telegram.sendMessage(ctx.chat.id, currentStopMessage);
+            bot.telegram.sendMessage(chatId, currentStopMessage);
 
             currentStopIndex = (currentStopIndex + 1) % stopMsgs.length;
         } else {
@@ -85,7 +80,7 @@ bot.on("text", (ctx) => {
         }
     } else {
         const randomExcuse = excuses[Math.floor(Math.random() * excuses.length)];
-        bot.telegram.sendMessage(ctx.chat.id, randomExcuse);
+        bot.telegram.sendMessage(chatId, randomExcuse);
     }
 });
 
@@ -93,7 +88,6 @@ const WEBHOOK_URL = `${process.env.SERVER_URL}/webhook/${process.env.BOT_TOKEN}`
 bot.telegram.setWebhook(WEBHOOK_URL).then(() => {
     console.log(`✅ Webhook установлен: ${WEBHOOK_URL}`);
 });
-
 
 const PORT = 7001;
 app.listen(PORT, () => {
